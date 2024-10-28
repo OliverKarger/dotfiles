f33186ad8b8b1fcc5724364561054bcf26f7eb79
Oliver Karger <oliverkarger@proton.me>
2024-10-28 12:52:06 +0100

    Working on install Script

--- install.zsh
+++ install.zsh
@@ -3,14 +3,14 @@
 # Check if zsh is installed
 if [ ! -f /usr/bin/zsh ];
 then
-	echo "zsh needs tobe installed!" >&2
+	echo "zsh needs to be installed!" >&2
 	exit 1
 fi
 
-# alacritty
+# yay package manager
+# 	required for AUR packages
+pacman -S git base-devel yay
 
-# neovim
-
-# spacenavd
-
-# oh-my-zsh
+# yay/pacman packages
+pacman -S alacritty neovim
+yay install spacenavd
