c359046a310099955cbce63fdfc6d325edf7b495
oliver.karger <oliver.karger@muetec.com>
2024-11-11 11:03:39 +0100

    Updated install-config.zsh with Prompts

--- install-config.zsh
+++ install-config.zsh
@@ -8,62 +8,93 @@ then
 fi
 
 # zsh
-if [ -f ~/.zshrc ]; 
+echo "Install ZSH Config? (y/n) "
+read zsh_yn
+if [ $zsh_yn -eq "y" ];
 then
-	# if a file exists, create a backup
-	rm ~/.zshrc
+    if [ -f ~/.zshrc ]; 
+    then
+	    # if a file exists, create a backup
+	    rm ~/.zshrc
+    fi
+    ln -s $(pwd)/.zshrc ~/.zshrc
 fi
-ln -s $(pwd)/.zshrc ~/.zshrc
 
 # neovim
-mkdir -p ~/.config/nvim/vim-plug
-
-if [ -f ~/.config/nvim/init.vim ]; 
+echo "Install Neovim Config? (y/n) "
+read neovim_yn
+if [ $neovim_yn -eq "y" ];
 then
-	rm -f ~/.config/nvim/init.vim
-fi
-ln -s $(pwd)/nvim/init.vim ~/.config/nvim/init.vim
+    mkdir -p ~/.config/nvim/vim-plug
 
-if [ -f ~/.config/nvim/vim-plug/plugins.vim ]; 
+    if [ -f ~/.config/nvim/init.vim ]; 
+    then
+	    rm -f ~/.config/nvim/init.vim
+    fi
+    ln -s $(pwd)/nvim/init.vim ~/.config/nvim/init.vim
+
+    if [ -f ~/.config/nvim/vim-plug/plugins.vim ]; 
+    then
+	    rm -f ~/.config/nvim/vim-plug/plugins.vim
+    fi
+    ln -s $(pwd)/nvim/vim-plug/plugins.vim ~/.config/nvim/vim-plug/plugins.vim
+fi
+
+echo "Install Alacritty Config? (y/n) "
+read alacritty_yn
+if [ $alacritty_yn -eq "y" ];
 then
-	rm -f ~/.config/nvim/vim-plug/plugins.vim
+    mkdir -p ~/.config/alacritty/themes
+
+    if [ -f ~/.config/alacritty/alacritty.toml ]; 
+    then
+	    rm -f ~/.config/alacritty/alacritty.toml
+    fi
+
+    ln -s $(pwd)/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+
+    if [ -f ~/.config/alacritty/themes/ayu_dark.toml ]; 
+    then
+	    rm -f ~/.config/alacritty/themes/ayu_dark.toml
+    fi
+
+    ln -s $(pwd)/alacritty/themes/ayu_dark.toml ~/.config/alacritty/themes/ayu_dark.toml
+
+    if [ -f ~/.config/alacritty/themes/gruvbox_material_dark.toml ]; 
+    then
+	    rm -f ~/.config/alacritty/themes/gruvbox_material_dark.toml
+    fi
+
+    ln -s $(pwd)/alacritty/themes/gruvbox_material_dark.toml ~/.config/alacritty/themes/gruvbox_material_dark.toml
 fi
-ln -s $(pwd)/nvim/vim-plug/plugins.vim ~/.config/nvim/vim-plug/plugins.vim
 
-# alacritty
-mkdir -p ~/.config/alacritty/themes
-
-if [ -f ~/.config/alacritty/alacritty.toml ]; 
+echo "Install spacenavd Config? (y/n) "
+read spacenavd_yn
+if [ $spacenavd_yn -eq "y" ];
 then
-	rm -f ~/.config/alacritty/alacritty.toml
-fi
-ln -s $(pwd)/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+    if [ -f /etc/spnavrc ]; 
+    then
+	    rm /etc/spnavrc	
+    fi
+    
+    ln -s $(pwd)/spnavrc /etc/spnavrc
 
-if [ -f ~/.config/alacritty/themes/ayu_dark.toml ]; 
+    echo "Reload spacenavd? (y/n)"
+    read reload_spacenavd_yn
+    if [ $reload_sapcenavd_yn -eq "y" ];
+    then
+        systemctl daemon-reload
+        systemctl restart spacenavd
+    fi
+fi
+
+echo "Install FanControl Config? (y/n) "
+read fancontrol_yn
+if [ $fancontrol_yn -eq "y" ];
 then
-	rm -f ~/.config/alacritty/themes/ayu_dark.toml
+    if [ -f /etc/fancontrol ];
+    then
+	    rm /etc/fancontrol
+    fi
+    ln -s $(pwd)/fancontrol /etc/fancontrol
 fi
-ln -s $(pwd)/alacritty/themes/ayu_dark.toml ~/.config/alacritty/themes/ayu_dark.toml
-
-if [ -f ~/.config/alacritty/themes/gruvbox_material_dark.toml ]; 
-then
-	rm -f ~/.config/alacritty/themes/gruvbox_material_dark.toml
-fi
-ln -s $(pwd)/alacritty/themes/gruvbox_material_dark.toml ~/.config/alacritty/themes/gruvbox_material_dark.toml
-
-# spacenavd
-if [ -f /etc/spnavrc ]; 
-then
-	rm /etc/spnavrc	
-fi
-ln -s $(pwd)/spnavrc /etc/spnavrc
-
-systemctl daemon-reload
-systemctl restart spacenavd
-
-# fancontrol
-if [ -f /etc/fancontrol ];
-then
-	rm /etc/fancontrol
-fi
-ln -s $(pwd)/fancontrol /etc/fancontrol
