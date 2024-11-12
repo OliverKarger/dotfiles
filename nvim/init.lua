66cb9d38d7749ab8013c3fc27d2f4aa7bbb63772
Oliver Karger <oliverkarger@proton.me>
2024-11-12 16:14:33 +0100

    Added Plugins Section to init.lua and removed old vimscript configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,26 +1,48 @@
--- Load Plugins
--- Assuming you're using vim-plug in the `vim-plug/plugins.vim` file
-vim.cmd('source ' .. vim.fn.expand('~') .. '/.config/nvim/vim-plug/plugins.vim')
+-- Plugins
+local vim = vim
+local Plug = vim.fn['plug#']
 
--- --- Settings: Start ---
+vim.call('plug#begin')
+
+Plug('sheerun/vim-polyglot')
+Plug('neoclide/coc.nvim', { ['branch'] = 'release' })
+Plug('jiangmiao/auto-pairs')
+Plug('morhetz/gruvbox')
+Plug('williamboman/mason.nvim')
+Plug('williamboman/mason-lspconfig.nvim')
+Plug('nvim-lualine/lualine.nvim')
+Plug('nvim-tree/nvim-web-devicons')
+Plug('nvim-lua/plenary.nvim')
+Plug('nvim-telescope/telescope.nvim')
+Plug('nvim-telescope/telescope-file-browser.nvim')
+Plug('junegunn/fzf', { ['do'] = function() 
+	vim.fn['fzf#install']()
+	end })
+Plug('junegunn/fzf.vim')
+Plug('akinsho/toggleterm.nvim', { ['tag'] = '*' })
+Plug('hrsh7th/vim-vsnip')
+Plug('hrsh7th/vim-vsnip-integ')
+
+vim.call('plug#end')
+
+
+-- Global Settings
 vim.o.termguicolors = true
 vim.wo.number = true
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.cmd('filetype indent on')
 
--- Disable netrw (default file explorer in Neovim)
+-- Disable netrw (default Neovim Directory Browser)
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwSettings = 1
 vim.g.loaded_netrwFileHandlers = 1
--- --- Settings: End ---
 
--- --- Toggleterm: Start ---
+-- Toggleterm
 require("toggleterm").setup({})
--- --- Toggleterm: End ---
 
--- --- Telescope: Start ---
+-- Telescope File Browser
 require('telescope').setup {
   defaults = {
     find_command = {
@@ -36,13 +58,11 @@ require('telescope').setup {
     }
   }
 }
-
--- Key mappings for Telescope
+-- Key Mappings for Telescope File Browser
 vim.api.nvim_set_keymap('n', '<C-e>', ':Telescope file_browser<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-s>', ':Telescope find_files<CR>', { noremap = true, silent = true })
--- --- Telescope: End ---
 
--- --- LSP: Start ---
+-- Mason
 require("mason").setup({
   ui = {
     icons = {
@@ -52,27 +72,25 @@ require("mason").setup({
     }
   }
 })
+-- Mason LSP
 require('mason-lspconfig').setup({
 	automatic_installation = true,
-	ensure_installed = { "csharpier", "lua_ls", "clangd", "cmakelang", "bashls", "docker_compose_language_service" }
+	ensure_installed = { "omnisharp", "lua_ls", "clangd", "bashls", "docker_compose_language_service" }
 })
--- --- LSP: End ---
 
--- --- Color Scheme: Start ---
+-- Lualine
+require('lualine').setup({
+	options = {
+		icons_enabled = true,
+		theme = 'gruvbox-material'
+	}
+})
+
+-- Colorscheme
 vim.cmd('colorscheme gruvbox')
 vim.o.background = 'dark'
-vim.g.airline_theme = 'wombat'
 
--- Key mappings for Gruvbox highlighting
-vim.api.nvim_set_keymap('n', '[oh', ':call gruvbox#hls_show()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', ']oh', ':call gruvbox#hls_hide()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'coh', ':call gruvbox#hls_toggle()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '*', ':let @/ = ""<CR>:call gruvbox#hls_show()<CR>*', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '/', ':let @/ = ""<CR>:call gruvbox#hls_show()<CR>/', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '?', ':let @/ = ""<CR>:call gruvbox#hls_show()<CR>?', { noremap = true, silent = true })
--- --- Color Scheme: End ---
-
--- --- Window Navigation: Start ---
+-- Window Navigation
 vim.api.nvim_set_keymap('n', '<C-w><Up>', '<C-w>k', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-w><Down>', '<C-w>j', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-w><Left>', '<C-w>h', { noremap = true, silent = true })
@@ -80,5 +98,4 @@ vim.api.nvim_set_keymap('n', '<C-w><Right>', '<C-w>l', { noremap = true, silent 
 vim.api.nvim_set_keymap('n', '<C-w>v', ':vsplit<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-w>h', ':split<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-w>T', ':term<CR>', { noremap = true, silent = true })
--- --- Window Navigation: End ---
 
