fe181e2595a2ac3dca19b6ba990a8b25f1497d5b
Oliver Karger <oliverkarger@proton.me>
2024-11-12 17:22:49 +0100

    restructed init.lua to be more "safe" :)

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,12 @@
+-- "safe" require wrapper
+local function safe_require(plugin)
+	local success, result = pcall(require, plugin)
+	if not success then
+		print("Plugin Error: " .. plugin)
+	end
+	return result
+end
+
 -- Plugins
 local vim = vim
 local Plug = vim.fn['plug#']
@@ -23,9 +32,14 @@ Plug('akinsho/toggleterm.nvim', { ['tag'] = '*' })
 Plug('hrsh7th/vim-vsnip')
 Plug('hrsh7th/vim-vsnip-integ')
 Plug('scottmckendry/cyberdream.nvim')
+Plug('mfussenegger/nvim-dap')
+Plug('mfussenegger/nvim-lint')
+Plug('nvim-neotest/nvim-nio')
+Plug('rcarriga/nvim-dap-ui')
+Plug('mhartington/formatter.nvim')
+Plug('nvim-pack/nvim-spectre')
 
 vim.call('plug#end')
-
 
 -- Global Settings
 vim.o.termguicolors = true
@@ -40,11 +54,39 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwSettings = 1
 vim.g.loaded_netrwFileHandlers = 1
 
--- Toggleterm
-require("toggleterm").setup({})
+-- Window Navigation
+vim.api.nvim_set_keymap('n', '<C-w><Up>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-w><Down>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-w><Left>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-w><Right>', '<C-w>l', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-w>v', ':vsplit<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-w>h', ':split<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-w>T', ':term<CR>', { noremap = true, silent = true })
+
+-- Spectre Search
+vim.keymap.set('n', '<C-f>s', '<cmd>lua require("spectre").toggle()<CR>', {
+	desc = "Toggle Spectre"		
+})
+vim.keymap.set('n', '<C-f>w', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+		desc = "Search current Word"
+})
+
+-- Colorscheme
+vim.cmd('colorscheme cyberdream')
+vim.o.background = 'dark'
+
+-- Lualine
+safe_require('lualine').setup({
+	options = {
+		icons_enabled = true,
+		theme = 'auto',
+		component_separators = { left = '', right = '' },
+		section_separators = { left = '', right = '' }
+	}
+})
 
 -- Telescope File Browser
-require('telescope').setup {
+safe_require('telescope').setup {
   defaults = {
     find_command = {
       'rg',
@@ -63,8 +105,11 @@ require('telescope').setup {
 vim.api.nvim_set_keymap('n', '<C-e>', ':Telescope file_browser<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-s>', ':Telescope find_files<CR>', { noremap = true, silent = true })
 
+-- Toggleterm
+safe_require("toggleterm").setup({})
+
 -- Mason
-require("mason").setup({
+safe_require("mason").setup({
   ui = {
     icons = {
       package_installed = "✓",
@@ -73,32 +118,12 @@ require("mason").setup({
     }
   }
 })
+
 -- Mason LSP
-require('mason-lspconfig').setup({
+safe_require('mason-lspconfig').setup({
 	automatic_installation = true,
 	ensure_installed = { "omnisharp", "lua_ls", "clangd", "bashls", "docker_compose_language_service" }
 })
 
--- Lualine
-require('lualine').setup({
-	options = {
-		icons_enabled = true,
-		theme = 'auto',
-		component_separators = { left = '', right = '' },
-		section_separators = { left = '', right = '' }
-	}
-})
-
--- Colorscheme
-vim.cmd('colorscheme cyberdream')
-vim.o.background = 'dark'
-
--- Window Navigation
-vim.api.nvim_set_keymap('n', '<C-w><Up>', '<C-w>k', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-w><Down>', '<C-w>j', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-w><Left>', '<C-w>h', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-w><Right>', '<C-w>l', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-w>v', ':vsplit<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-w>h', ':split<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-w>T', ':term<CR>', { noremap = true, silent = true })
-
+-- Formatter
+safe_require('formatter').setup()
