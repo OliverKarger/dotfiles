4c8923cc8bc267930de69d4e1ff00ad636e9dae2
Oliver Karger <oliverkarger@proton.me>
2025-04-23 23:23:17 +0200

    removed old files

--- nvim/lua/spectre_config.lua
+++
@@ -1,6 +0,0 @@
-local _M = {}
-
-_M.setup = function()
-end
-
-return _M
--- nvim/lua/tasks.lua
+++
@@ -1,8 +0,0 @@
-local utils = require('utils')
-local _M = {}
-
-_M.setup = function()
-  utils.safe_require('overseer').setup()
-end
-
-return _M
